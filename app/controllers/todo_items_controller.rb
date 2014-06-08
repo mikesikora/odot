034a29d0441388c4d7d7b15511ab38c04f74a235
Mike Sikora <msikora.umd@gmail.com>
2014-06-08 18:39:43 -0400

    hello

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -2,4 +2,8 @@ class TodoItemsController < ApplicationController
   def index
   	@todo_list = TodoList.find(params[:todo_list_id])
   end
+  def new
+  	@todo_list = TodoList.find(params[:todo_list_id])
+  	@todo_item = @todo_list.todo_items.new
+  end
 end
